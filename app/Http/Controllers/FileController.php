8b58e36fd64fe6e60228bf44f624182b13d15b96
saleksak <wojtekqq@tlen.pl>
2022-11-24 15:40:03 +0100

    dodane generowanie pdf

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use DirectoryIterator;
+use Dompdf\Dompdf;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
@@ -35,6 +36,7 @@ class FileController extends Controller
     }
     public function upload(Request $request)
     {
+        set_time_limit(0);
         $userId = $request->user()->id;
         $uploadedFile = $request->file('file');
         $filename = time().$uploadedFile->getClientOriginalName();
@@ -53,7 +55,7 @@ class FileController extends Controller
         $folderName = pathinfo($storageDestinationPath, PATHINFO_FILENAME);
 //        $storageDestinationPath=  $path . $userId . '/' . $folderName ;
 //        $xml = $path . $userId .  '/upload/' . $folderName . '/BDOT10k/PL.PZGiK.330.1425__OT_ADJA_A.xml';
-        $xsd = $path . $userId .  '/upload/' . $folderName . '/BDOT10kGML/OT_BDOT10k_BDOO.xsd';
+        $xsd = $path . $userId .  '/upload/' . $folderName . '/XSD/OT_BDOT10k_BDOO.xsd';
         $storageDestinationPath=  $path . $userId . '/upload' ;
 
 
@@ -92,29 +94,35 @@ class FileController extends Controller
 //        var_dump($dir); die();
 
 
-        $raport = file_get_contents(public_path('raport.html'));
+        $reportProd = $path . $userId .  '/upload/report.html';
+        $raport = file_get_contents(public_path('report.html'));
         $raport = str_replace('NAZWA_ZBIORU',$folderName, $raport);
-        file_put_contents($path . $userId .  '/upload/Raport.html', $raport);
+        file_put_contents($reportProd, $raport);
 
 
 // Validate
         $xmlValidator = new XmlValidator();
 
         $dir = new DirectoryIterator($path . $userId .  '/upload/' . $folderName . '/BDOT10k/');
-        foreach ($dir as $fileinfo) {
 
+        foreach ($dir as $fileinfo) {
+            $error_value = null;
+            $breakLine = '<br>';
 
             if (!$fileinfo->isDot()) {
+                $xmlFilename = $fileinfo->getfileName();
+                $reportName = file_get_contents(public_path('report_file_name.html'));
+                $reportName = str_replace('XML_FILE_NAME',$xmlFilename, $reportName);
+                file_put_contents($reportProd, $reportName, FILE_APPEND);
 //                var_dump($fileinfo->getpathName()); die();
-//                $xml =  file_get_contents($path . $userId .  '/upload/' . $folderName . '/BDOT10k/PL.PZGiK.330.1425__OT_ADJA_A.xml');
-                $xml =  file_get_contents($fileinfo->getpathname());
+                $xml =  file_get_contents($path . $userId .  '/upload/' . $folderName . '/BDOT10k/' . $xmlFilename);
+//                $xml =  file_get_contents($fileinfo->getpathname());
                 try{
 
                     $xmlValidator->validate($xml,$xsd);
-                    $newArray = array();
-
                     // Check if is valid
                     if(!$xmlValidator->isValid()){
+
 
                         // Do whatever with the errors.
                         foreach ($xmlValidator->errors as $error) {
@@ -122,29 +130,57 @@ class FileController extends Controller
 //                        $error->level, $error->code, $error->message,
 //                        $error->file, $error->line, $error->column
 //                    );
-                            $newArray[] = $error;
-
+                        $error_value = 'Linia nr ' . $error->line . ' ' . $error->message . $breakLine;
+                        $reportError = file_get_contents(public_path('report_error_data.html'));
+                        $reportError = str_replace('ERROR_DATA',$error_value, $reportError);
+                        file_put_contents($reportProd, $reportError, FILE_APPEND);
+//                        die();
                         }
+                        $reportError = file_get_contents($reportProd);
+                        $reportError = str_replace('STATUS_RAPORTU','Negatywny', $reportError);
+                        file_put_contents($reportProd, $reportError,);
+                        $reportError = file_get_contents($reportProd);
+                        $reportError = str_replace('green','red', $reportError);
+                        file_put_contents($reportProd, $reportError);
 
-//var_dump($newArray); die();
-                        return back()->with('message', 'nie');
-//                return back()->with('message', 'Your file is bad');
+                    } else {
+                        $error_value = 'Nie stwierdzono błędów';
+                        file_put_contents($reportProd, $error_value, FILE_APPEND);
+//                        die();
                     }
+                    $reportError = file_get_contents($reportProd);
+                    $reportError = str_replace('STATUS_RAPORTU','Negatywny', $reportError);
+                    file_put_contents($reportProd, $reportError,);
+
                 } catch (\InvalidArgumentException $e){
                     // catch InvalidArgumentException
                 }
             }
 
-
-
         }
-//die();
+
+        $reportEnd = file_get_contents(public_path('report_end.html'));
+          file_put_contents($reportProd, $reportEnd, FILE_APPEND);
+        $reportEnd = file_get_contents($reportProd);
+
+// instantiate and use the dompdf class
+        $dompdf = new Dompdf();
+        $dompdf->loadHtml($reportEnd);
+
+// (Optional) Setup the paper size and orientation
+        $dompdf->setPaper('A4', 'landscape');
+
+// Render the HTML as PDF
+        $dompdf->render();
+
+// Output the generated PDF to Browser
+//        $dompdf->stream();
 
 
 
 
-
-        return back()->with('message', 'Your file is valid');
+        return $dompdf->stream();
+//        return back()->with('message', 'Your file is valid');
     }
 
 
