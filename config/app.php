91ec9e27e0d2b304c721dc001d32f438b5641e2a
saleksak <wojtekqq@tlen.pl>
2022-11-27 21:48:24 +0100

    modyfikacja strony startowej

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Walidator'),
+    'name' => env('APP_NAME', 'Walidator BDOT10k'),
 
     /*
     |--------------------------------------------------------------------------
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,8 +9,8 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>@hasSection('template_title')@yield('template_title') | @endif {{ config('app.name', Lang::get('titles.app')) }}</title>
-        <meta name="description" content="">
-        <meta name="author" content="Jeremy Kenedy">
+{{--        <meta name="description" content="">--}}
+{{--        <meta name="author" content="Jeremy Kenedy">--}}
         <link rel="shortcut icon" href="/favicon.ico">
 
         {{-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries --}}
